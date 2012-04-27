f7d1425b1fd4ed1c7c630c09752db283f1080778
Jim Thomason <jim@jimandkoka.com>
2012-04-27 09:01:39 -0500

    updated to new file format

--- scripts/transcript_ontology_exporter.pl
+++ scripts/transcript_ontology_exporter.pl
@@ -1,13 +1,5 @@
 #!/usr/local/bin/perl
 
-# spits out transcript ontology links (accession, term, linkage type) to STDOUT. 
-# give it options :
-#
-# --registry (registry file to use (defaults to Gramene registry file in known location)
-# --species (species you're hitting)
-# --transcript (the transcript ID to dump)
-
-
 use lib '/usr/local/gramene/lib/perl';
 
 
@@ -33,28 +25,65 @@ GetOptions(
 Bio::EnsEMBL::Registry->load_all($args{'registry'});
 
 my $transcriptA = Bio::EnsEMBL::Registry->get_adaptor($args{'species'}, 'Core', 'Transcript');
+my $goA         = Bio::EnsEMBL::Registry->get_adaptor('Multi', 'ontology', 'goterm');
 
-
-my $transcript = $transcriptA->fetch_by_stable_id($args{'transcript'});
+my $transcripts = $args{'transcript'}
+    ? [$transcriptA->fetch_by_stable_id($args{'transcript'})]
+    : $transcriptA->fetch_all;
 
 my $delim = "\t";
 my $altdelim = ",";
 
-print join($delim, qw(Accession Term Linkage)), "\n";  
+print join($delim, qw( TranscriptID OntologyID OntologyDescription OntologyEvidenceCode OntologyType )), "\n";  
 
-my $links = $transcript->get_all_DBLinks();
-foreach my $link (@$links) {
-    next unless (ref $link) =~ /OntologyXref/;
-    print 
-        join(
-            $delim,
-                $link->display_id,
-                $link->description,
+foreach my $transcript (@$transcripts) {
+
+    my $links = $transcript->get_all_DBLinks();
+    foreach my $link (@$links) {
+        next unless (ref $link) =~ /OntologyXref/;
+        print 
+            join(
+                $delim,
+                    $transcript->display_id,
+                    $link->display_id,
+                    $link->description,
+                    join(
+                        $altdelim,
+                            @{$link->get_all_linkage_types}
+                    ),
+                    $link->dbname,
+            ),
+            "\n";
+            
+        my $ontology_term = $goA->fetch_by_accession($link->display_id);
+        foreach my $slimterm ( @{ $ontology_term->ancestors } ) {
+            my $s = join(',', @{$slimterm->subsets});
+            next unless $s =~/goslim_generic/;
+            #print Dumper($slimterm); last; use Data::Dumper;
+            print 
                 join(
-                    $altdelim,
-                        @{$link->get_all_linkage_types}
-                )
-        ),
-        "\n";
+                    $delim,
+                        $transcript->display_id,
+                        $slimterm->accession,
+                        $slimterm->name,
+                        '', #use term evidence code?
+                        $slimterm->ontology,
+                ),
+                "\n";
+        }
+    }
 }
+
+=pod
+
+=head1 DESCRIPTION
+
+spits out transcript ontology links (accession, term, linkage type) to STDOUT. 
+give it options :
+
+ --registry (registry file to use (defaults to Gramene registry file in known location)
+ --species (species you're hitting)
+ --transcript (the transcript ID to dump) (optional. If omitted will dump all transcripts)
+
+=cut
 
