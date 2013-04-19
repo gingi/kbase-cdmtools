58f698b803a7a5bc64a6192d7247cdebc03346af
Shiran Pasternak <shiranpasternak@gmail.com>
2013-04-19 15:35:24 +0000

    More robust ID mapping; QC checks for protein IDs.

--- scripts/translations_id_remapper.pl
+++ scripts/translations_id_remapper.pl
@@ -1,89 +1,97 @@
+#!/usr/bin/env perl
+
+use strict;
 use warnings;
-use autodie;
 
 use Getopt::Long;
 use Text::RecordParser;
+use Pod::Usage;
 
-my %args = (mapfield => 'feature_id');
+MAIN: {
+    my %options = (mapfield => 'feature_id');
 
-GetOptions(\%args, 'features=s', 'translations=s', 'output=s', 'mapfield=s',);
+    GetOptions(\%options, 'features=s', 'translations=s', 'output=s',
+        'mapfield=s', 'help|h')
+        || pod2usage(-verbose => 1);
 
-die "No features file"    unless $args{features};
-die "No translation file" unless $args{translations};
-# warn "No output file specified...updating translations in place"
-#     unless $args{output};
+    pod2usage(-verbose => 1) if $options{help};
 
-$args{output} ||= $args{translations};
+    die "No features file"    unless $options{features};
+    die "No translation file" unless $options{translations};
 
-my $featuresP = Text::RecordParser->new($args{features});
-$featuresP->field_separator("\t");
-$featuresP->bind_fields(qw(feature_id type location parent subset name));
+    $options{output} ||= $options{translations};
 
-my $mapping               = {};
-my $transcript_id_mapping = {};
+    my $featuresP = Text::RecordParser->new($options{features});
+    $featuresP->field_separator("\t");
+    $featuresP->bind_fields(qw(feature_id type location parent subset name));
 
-while (my $feature = $featuresP->fetchrow_hashref) {
-    if ($feature->{type} eq 'mRNA') {
-        $transcript_id_mapping->{ $feature->{feature_id} }
-            = $feature->{feature_id};
-    }
-    
-    if ($feature->{type} eq 'CDS') {
-        if (defined $mapping->{ $feature->{parent} }) {
-            die "Multiple CDses provides for $feature->{parent}";
+    my %mapping;
+    my %ancestors = (undef => undef);
+
+    while (my $feature = $featuresP->fetchrow_hashref) {
+        my $id     = $feature->{feature_id};
+        my $parent = $feature->{parent};
+        $parent = undef if ($parent eq ".");
+        $mapping{$id}   = $id;
+        $ancestors{$id} = $parent;
+        while (defined $parent) {
+            $mapping{$parent} = $feature->{feature_id};
+            $parent = $ancestors{$parent};
         }
-        $mapping->{ $transcript_id_mapping->{ $feature->{parent} } }
-            = $feature->{ $args{mapfield} };
-    }
-    # Temporary hack by Shiran to fix translation dumps in place.
-    # if ($feature->{type} eq 'CDS') {
-    #     $mapping->{$feature->{name}} = $feature->{feature_id};
-    # }
-}
-
-my @output = ();
-
-open(my $translationsfh, "<", $args{translations});
-$/ = "\n>";
-
-while (my $record = <$translationsfh>) {
-    chomp($record);
-    my ($id, $sequence) = split /\n/, $record, 2;
-    $id =~ s/^>//;
-
-    if ($mapping->{$id}) {
-        $id = $mapping->{$id};
-    } else {
-        warn "No mapping provided for $id on line $.";
     }
 
-    push @output, ">$id\n$sequence";
+    my @output = ();
+
+    open(my $translationsfh, "<", $options{translations});
+    $/ = "\n>";
+
+    while (my $record = <$translationsfh>) {
+        chomp($record);
+        my ($id, $sequence) = split /\n/, $record, 2;
+        $id =~ s/^>//;
+
+        if ($mapping{$id}) {
+            $id = $mapping{$id};
+        }
+        else {
+            print STDERR "No mapping provided for $id on line $.\n";
+        }
+
+        push @output, ">$id\n$sequence";
+    }
+
+    close $translationsfh;
+
+    # warn "Overwriting $options{output}" if -e $options{output};
+
+    open(my $outputfh, ">", $options{output});
+    print $outputfh join("\n", @output);
+    close $outputfh;
 }
 
-close $translationsfh;
-
-# warn "Overwriting $args{output}" if -e $args{output};
-
-open(my $outputfh, ">", $args{output});
-print $outputfh join("\n", @output);
-close $outputfh;
-
-__DATA__
+1;
 
 =pod
 
-=head1 DESCRIPTION
+=head1 NAME
+
+    translations_id_mapper.pl
+
+=head1 SYNOPSIS
+
+    translations_id_mapper.pl [options]
 
 Given a features file and a translations file, will remap all transcript ids in
 the translation file to CDS ids provided in the parent file. Each transcript
 must be associated with only one CDS.
 
-Arguments:
+Options:
 
+ --help, -h      This help message
  --features      /path/to/features.file
  --translations  /path/to/translations.file
  --output        /path/to/output.file  (optional. Will default to --translations
                                         file and update in place if not set)
---mapfield       field to map transcript ids to. Defaults to "id"
+ --mapfield      field to map transcript ids to. Defaults to "feature_id"
 
 =cut
